d3fa8019ac9dbe709a7a75fb6a6143d181351a83
melanyvera2630 <84828555+melanyvera2630@users.noreply.github.com>
2025-01-17 08:25:54 -0500

    UPDATE COLORS

--- lib/ui/theme/colors.dart
+++ lib/ui/theme/colors.dart
@@ -14,6 +14,13 @@ const deepMagenta = Color(0xFF880158);
 const vividMagenta = Color(0xFFFF00AB);
 const teal = Color(0xFF008A74);
 
+// COLORS PROVIDED BY CONAGOPARE
+const intenseMagenta = Color(0x00CC198E);
+const neonMagenta = Color(0x00FF00AE);
+const paleLavanderPink = Color(0x00FFD2FF);
+const limeGreen = Color(0x00E3FFCA);
+const darkEmeraldGreen = Color(0x00004036);
+
 const hintColor = Color(0xFFA9A9A9);
 
 // GRADIENTS
@@ -32,6 +39,71 @@ const bgGradientLight = LinearGradient(
     Color(0xFFEAFED1),
     Color(0xFFFFD4FD),
   ],
+  begin: Alignment.bottomLeft,
+  end: Alignment.topRight,
+);
+
+//GRADIENT BUTTON 1
+const bgGradientButton1 = LinearGradient(
+  colors: [
+    Color(0x00BC308C),
+    Color(0x00FF00AB),
+  ],
+  //Falta 100% angulo 45
+  begin: Alignment.bottomLeft,
+  end: Alignment.topRight,
+);
+
+//GRADIENT BUTTON 2
+const bgGradientButton2 = LinearGradient(
+  colors: [
+    Color(0x00FFD2FF),
+    Color(0x00E3FFCA),
+  ],
+  //Falta 100% angulo 45
+  begin: Alignment.bottomLeft,
+  end: Alignment.topRight,
+);
+
+//GRADIENT HEADER
+const bgGradientHeader = LinearGradient(
+  colors: [
+    Color(0x00FF00AE),
+    Color(0x0094005A),
+  ],
+  //Falta 100% angulo 0
+  begin: Alignment.bottomLeft,
+  end: Alignment.topRight,
+);
+
+//GRADIENT NOTIFICATION
+const bgGradientNotification = LinearGradient(
+  colors: [
+    Color(0x00FFFFFF),
+  ],
+  //Falta 33%
+  begin: Alignment.bottomLeft,
+  end: Alignment.topRight,
+);
+
+//GRADIENT BACK PROFESIONAL
+const bgGradientBackProfesional = LinearGradient(
+  colors: [
+    Color(0x008AB7AC),
+    Color(0x00A5E5C8)
+  ],
+  //Falta angulo 0
+  begin: Alignment.bottomLeft,
+  end: Alignment.topRight,
+);
+
+//GRADIENT BACK CHATBOT
+const bgGradientBackChatbot = LinearGradient(
+  colors: [
+    Color(0x00964578),
+    Color(0x00F285CA)
+  ],
+  //Falta angulo 0
   begin: Alignment.bottomLeft,
   end: Alignment.topRight,
 );
